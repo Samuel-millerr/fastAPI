838f8bd23cfd017f3e0c7bf2fdc904f7e799e9b7
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2025-11-03 10:22:19 -0300

    chore: remoção da parte de files

--- api/v1/api.py
+++ api/v1/api.py
@@ -7,4 +7,3 @@ api_router = APIRouter()
 
 api_router.include_router(country_routes.router, prefix="/countries", tags=["Países"])
 api_router.include_router(country_statistics_routes.router, prefix="/statistics", tags=["Estatísticas"])
-api_router.include_router(files.router, prefix="/file", tags=["Files"])
